ae9782f615956b8b803d92f36418481ca454429c
Elton Zhang <zhang.elton@bcchsnyc.net>
2013-08-15 19:39:40 -0400

    New Files

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\user\avo1\avo\platforms\android\bin\AndroidManifest.xml : \
+C:\Users\user\avo1\avo\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,10 @@
+C:\Users\user\avo1\avo\platforms\android\bin\classes.dex : \
+C:\Users\user\avo1\avo\platforms\android\bin\classes\com\example\avo\Avo.class \
+C:\Users\user\avo1\avo\platforms\android\bin\classes\com\example\avo\BuildConfig.class \
+C:\Users\user\avo1\avo\platforms\android\bin\classes\com\example\avo\R$attr.class \
+C:\Users\user\avo1\avo\platforms\android\bin\classes\com\example\avo\R$drawable.class \
+C:\Users\user\avo1\avo\platforms\android\bin\classes\com\example\avo\R$layout.class \
+C:\Users\user\avo1\avo\platforms\android\bin\classes\com\example\avo\R$string.class \
+C:\Users\user\avo1\avo\platforms\android\bin\classes\com\example\avo\R$xml.class \
+C:\Users\user\avo1\avo\platforms\android\bin\classes\com\example\avo\R.class \
+C:\Users\user\avo1\avo\platforms\android\bin\dexedLibs\cordova-3.0.0-550f73f2b91676c8deb134e251c4c641.jar \
